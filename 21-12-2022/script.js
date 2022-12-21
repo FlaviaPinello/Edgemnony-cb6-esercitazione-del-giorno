26bdbf173e793d847d906aabb5facd6f5db23a68
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2022-12-21 17:52:36 +0100

    Esercitazione del 21-12-2022

---
+++ 21-12-2022/script.js
@@ -0,0 +1,19 @@
+//Sulla base della lezione del giorno:
+//scrivere una prima funzione che presa un stringa come argomento, ritorni la stessa stringa + la parola bootcamp;
+
+//function cb6(corso, another = "bootcamp") {
+//console.log (corso + another);
+//}
+//cb6 ("libro, penna ");
+
+//scrivere un oggetto col vostro nome che vi identifichi in proprietà (nome, cognome, ecc...)
+// e per metodi (es. possono anche essere semplici console.log di voi che eseguite un hobby)
+const CartaIdentità = {
+    nome: "Flavia",
+    cognome: "Pinello",
+    età: "37",
+    studia: function () {
+     console.log("Edgemony Bootcamp");
+     },
+    };
+console.log(CartaIdentità)
