3313622eb3ffc9477bc05af7c7b9d2be550e13df
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2023-01-11 17:52:36 +0100

    Esercitazione del 11 Gennaio 2023

---
+++ 11-01-2023/script.js
@@ -0,0 +1,29 @@
+
+// Creare una singola card tutto in statico (via HTML), inserire gli elementi a piacere, come immagini, testo o titoli, 
+// indispensabile pero aggiungere un button con una X come testo.
+
+// Aggiungere un evento al cui click del mouse sulla class intera:
+
+// Cambi il colore del testo di tutta la card
+// Aggiungere un secondo evento al cui click del mouse sul pulsante con la X - Elimini la card stessa
+
+
+
+function closeCard () {
+    cardClickEl.remove('card');
+}
+
+function changeColorText (){
+ cardClickEl.description.style.color = 'red';
+
+}
+
+
+
+const cardClickEl = document.querySelector('.card')
+cardClickEl.addEventListener('click', changeColorText);
+
+const btnXEl = document.querySelector('.btn');
+btnXEl.addEventListener('click', closeCard)
+
+
