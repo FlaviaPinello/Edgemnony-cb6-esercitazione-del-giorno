b6c168fbaee13f997e9a2e81634efee533c54954
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2023-03-01 18:19:35 +0100

    es 01-03-2023

---
+++ 01-03-2023/src/Main.jsx
@@ -0,0 +1,20 @@
+
+import { productsList } from "./mocks/productsList";
+import Control from "./components/control";
+import Hero from "./components/hero";
+// import Gallery from "./components/gallery";
+import ListProducts from "./components/listProducts/ListProducts";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <Hero title="Store" />
+      
+      <Control listDataLength={productsList.length} />
+      <ListProducts listData={productsList} />
+    </div>
+  );
+};
+
+export default Main;
---
+++ 01-03-2023/src/components/gallery/Gallery.jsx
@@ -0,0 +1,20 @@
+// import "./index.css";
+
+// const Gallery ({ images: [
+//     ,
+//     "https://picsum.photo/1200/300",
+//     "https://picsum.photo/1200/300",
+//     "https://picsum.photo/1200/300",
+//    ],
+//   }) =>{
+//     return(
+//      <div className = "Gallery">
+//         {imagesData.map(image)=>(
+//         <Gallery imageData = {image}/>
+//         )};
+//        ,
+//      </div>
+//     );
+// };
+
+// export default Gallery;
