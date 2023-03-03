90b77f64d5bb11f8d3498b40e237985ad2337a8c
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2023-03-03 01:05:18 +0100

    Esercitazione 02-03-2023

---
+++ 02-03-2023/src/Main.jsx
@@ -0,0 +1,43 @@
+
+import { useState } from "react";
+import { productsList } from "./mocks/productsList";
+import Control from "./components/control";
+import Hero from "./components/hero";
+import ImageProduct from "./components/imageProduct/ImageProduct";
+import ListProducts from "./components/listProducts/ListProducts";
+import Modal from "./components/modal";
+import Slider from "./components/slider";
+import "./main.css";
+
+const Main = () => {
+  const [isDarkMode, setDarkMode] = useState(true);
+  const [isModalOpen, setModalOpen] = useState(true);
+  const [singleProductModal, setSingleProductModal] = useState(null);
+
+  return (
+    <div className={`Main ${isDarkMode && "dark-mode"}`}>
+      <Hero title="Store" />
+      <Slider />
+      <Control listDataLength={productsList.length} />
+      <ListProducts
+        listData={productsList}
+        setSingleProductModal={setSingleProductModal}
+      />
+      <button
+        className="toggle-mode"
+        onClick={() => setDarkMode((prev) => !prev)}
+      >
+        {isDarkMode ? "☀️" : "🌙"}
+      </button>
+      {isModalOpen ? <Modal setModalOpen={setModalOpen} /> : null}
+      {singleProductModal ? (
+        <ImageProduct
+          product={singleProductModal}
+          setSingleProductModal={setSingleProductModal}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Main;
---
+++ 02-03-2023/src/components/listProducts/ListProducts.jsx
@@ -0,0 +1,20 @@
+
+
+import CardProduct from "../cardProduct";
+import "./index.css";
+
+const ListProducts = ({ listData, setSingleProductModal }) => {
+  return (
+    <div className="ListProducts">
+      {listData.map((product) => (
+        <CardProduct
+          productData={product}
+          setSingleProductModal={setSingleProductModal}
+          key={product.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ListProducts;
